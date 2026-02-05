ea945dba4b45b124f4aa8fe719f8cf7a4de565ed
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-02-05 12:54:42 -0500

    Switch to minitest-spec

--- Rakefile
+++ Rakefile
@@ -3,7 +3,11 @@
 require "bundler/gem_tasks"
 require "minitest/test_task"
 
-Minitest::TestTask.create
+Minitest::TestTask.create do |t|
+  t.libs.delete("test")
+  t.libs << "spec"
+  t.test_globs = ["spec/*_spec.rb"]
+end
 
 require "rubocop/rake_task"
 
---
+++ spec/type_toolkit_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module TypeToolkit
+  class TypeToolkitSpec < Minitest::Spec
+    it "has a version number" do
+      refute_nil ::TypeToolkit::VERSION
+    end
+
+    it "does something useful" do
+      assert false
+    end
+  end
+end
