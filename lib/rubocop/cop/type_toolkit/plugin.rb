f54a4eca6713ecd41f26627d3e5616a8e4473f99
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-03-03 17:13:27 -0500

    Migrate to RuboCop plugin system
    https://docs.rubocop.org/rubocop/latest/plugin_migration_guide.html

---
+++ lib/rubocop/cop/type_toolkit/plugin.rb
@@ -0,0 +1,33 @@
+# typed: false
+# frozen_string_literal: true
+
+require "lint_roller"
+
+module RuboCop
+  module Cop
+    module TypeToolkit
+      class Plugin < LintRoller::Plugin
+        def about
+          LintRoller::About.new(
+            name: "rubocop-type_toolkit",
+            version: ::TypeToolkit::VERSION,
+            homepage: "https://github.com/Shopify/type_toolkit",
+            description: "Detects misuse of UnexpectedNilError.",
+          )
+        end
+
+        def supported?(context)
+          context.engine == :rubocop
+        end
+
+        def rules(_context)
+          LintRoller::Rules.new(
+            type: :path,
+            config_format: :rubocop,
+            value: Pathname.new(__dir__).join("../../../../config/default.yml"),
+          )
+        end
+      end
+    end
+  end
+end
