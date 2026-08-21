4365dd32c05cb78fe2342984fbb0e7f9e4540b12
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-08-20 21:30:31 -0400

    WIP: Implement `extend` for interfaces

--- lib/type_toolkit/interface.rb
+++ lib/type_toolkit/interface.rb
@@ -26,10 +26,38 @@ module TypeToolkit
   module Interface
     #: (Module[top]) -> void
     def included(target_module)
+      install_onto(target_module)
+    end
+
+    #: (Module[top]) -> void
+    def extended(target_module)
+      install_onto(target_module.singleton_class)
+    end
+
+    private
+
+    #: (Module[top]) -> void
+    def install_onto(target_module)
       # Including/extending a module is idempotent, so we don't have to worry these were already included/extended.
 
       # Potentially abstract methods will be called on instances of `self`, so we need the `method_missing` hooks.
-      target_module.include(TypeToolkit::AbstractInstanceMethodReceiver)
+      if target_module.singleton_class?
+        # Handle cases like:
+        #
+        #     class C
+        #       class << self
+        #         include TheInterface
+        #       end
+        #     end
+        target_module.include(TypeToolkit::AbstractClassMethodReceiver)
+      else
+        # Handle the usual cases like:
+        #
+        #     class C
+        #       include TheInterface
+        #     end
+        target_module.include(TypeToolkit::AbstractInstanceMethodReceiver)
+      end
 
       # The `method_missing` hooks need to be able to look up the abstract methods (from the interface).
       target_module.extend(TypeToolkit::HasAbstractMethods)
